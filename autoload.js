a0ae3edbfed30684a0351abcba404d5aaffb9a8d
xjystudio <xiejiayu55@outlook.com>
2022-06-19 23:38:09 +0800

    flies

---
+++ autoload.js
@@ -0,0 +1,34 @@
+const live2d_path = "https://xjystudio.coding.net/p/blog/d/flies/git/raw/master/js/";
+function loadExternalResource(url, type) {
+	return new Promise((resolve, reject) => {
+		let tag;
+		if (type === "css") {
+			tag = document.createElement("link");
+			tag.rel = "stylesheet";
+			tag.href = url;
+		}
+		else if (type === "js") {
+			tag = document.createElement("script");
+			tag.src = url;
+		}
+		if (tag) {
+			tag.onload = () => resolve(url);
+			tag.onerror = () => reject(url);
+			document.head.appendChild(tag);
+		}
+	});
+}
+if (screen.width >= 600) {
+	Promise.all([
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+	]).then(() => {
+		initWidget({
+			waifuPath: live2d_path + "waifu-tips.json",
+			cdnPath: "https://cdn.xiejiayu.ml/gh/fghrsh/live2d_api/"
+		});
+	});
+}
+console.log(`
+`);
