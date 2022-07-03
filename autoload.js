fea756e093474cea0eed676731a8b1b2052639a7
xjystudio <xiejiayu55@outlook.com>
2022-07-03 20:51:46 +0800

    js

--- autoload.js
+++ autoload.js
@@ -1,17 +1,34 @@
-const live2d_path = "https://cdn.jsdelivr.net/gh/Xjystudio/js@master/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
 		let tag;
-		if (type === "css") {tag = document.createElement("link");tag.rel = "stylesheet";tag.href = url;}
-		else if (type === "js") {tag = document.createElement("script");tag.src = url;}
-		if (tag) {tag.onload = () => resolve(url);tag.onerror = () => reject(url);document.head.appendChild(tag);}});}
+
+		if (type === "css") {
+			tag = document.createElement("link");
+			tag.rel = "stylesheet";
+			tag.href = url;
+		}
+		else if (type === "js") {
+			tag = document.createElement("script");
+			tag.src = url;
+		}
+		if (tag) {
+			tag.onload = () => resolve(url);
+			tag.onerror = () => reject(url);
+			document.head.appendChild(tag);
+		}
+	});
+}
 if (screen.width >= 600) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-		loadExternalResource(live2d_path + "fireworks.js", "js")
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"});});}
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
+		});
+	});
+}
